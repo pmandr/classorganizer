32ab0c8dd13770b5e36ca507634a193f121499fb
paulomariomariomario <paulomariomariomario@gmail.com>
2012-05-05 21:38:04 +0000

    final merge without checking

--- classorg/general/functions.js
+++
@@ -1,9 +0,0 @@
-function validateLoginRequest(){
-	if(''==document.getElementById('login').value){
-		alert("O login não foi inserido!");return;
-	}
-	if(''==document.getElementById('password').value){
-		alert("A senha não foi inserida!");return;
-	}
-	document.getElementById('form_login').submit();
-}
